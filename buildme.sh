f8dda6b10f3030b383e5856bd4df8f89f0652a43
xtcedro <84252067+xtcedro@users.noreply.github.com>
2023-03-11 12:49:35 -0600

    Update buildme.sh

--- buildme.sh
+++ buildme.sh
@@ -8,13 +8,16 @@ mkdir -p ~/.local/src
 sudo apt update -yyq  
 sudo apt dist-upgrade -yyq
 sudo apt full-upgrade -yyq
+sudo apt autoremove -yyq
 
+# Install unattended-upgrades and set dpkg to update automatically
+sudo apt install unattended-upgrades -yyq && sudo dpkg-reconfigure --priority=low unattended-upgrades
 
 # Install necessary build dependencies
-sudo apt-get install -y build-essential libx11-dev libxft-dev libxinerama-dev libharfbuzz-dev libxext-dev libxcb1-dev libxcb-res0-dev
+sudo apt-get install -yyq build-essential libx11-dev libxft-dev libxinerama-dev libharfbuzz-dev libxext-dev libxcb1-dev libxcb-res0-dev
 
 # Install Neovim, Vim, Xinit, Picom, Xcompmgr, and Dialog
-sudo apt install -y neovim vim xinit picom xcompmgr dialog xorg ufw fail2ban vlc smplayer chromium-browser lynx neofetch newsboat neomutt -yyq
+sudo apt install -yyq neovim vim xinit picom xcompmgr dialog xorg ufw fail2ban vlc smplayer chromium-browser lynx neofetch newsboat neomutt -yyq
 
 # Clone all suckless programs into ~/.local/src
 cd ~/.local/src
@@ -22,14 +25,27 @@ git clone https://git.suckless.org/dwm
 git clone https://git.suckless.org/st
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/slock
+git clone https://git.suckless.org/slstatus
+git clone https://git.suckless.org/sent
+git clone https://git.suckless.org/tabbed
+git clone https://git.suckless.org/sic
+git clone https://git.suckless.org/surf
+git clone https://git.suckless.org/ii
 
 # Compile each program
 cd dwm && sudo make clean install && cd ..
 cd st && sudo make clean install && cd ..
 cd dmenu && sudo make clean install && cd ..
 cd slock && sudo make clean install && cd ..
+cd slstatus && sudo make clean install && cd ..
+cd sent && sudo make clean install && cd ..
+cd tabbed && sudo make clean install && cd ..
+cd sic && sudo make clean install && cd ..
+cd surf && sudo make clean install && cd ..
+cd ii && sudo make clean install && cd ..
 
 # Create .xinitrc file
+
 echo "exec dwm" > ~/.xinitrc
 
 # Change permissions on .xinitrc file
